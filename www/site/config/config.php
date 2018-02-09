f128069faaa98b5f6813013a5884d3f76d47b280
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-09 16:10:47 +1100

    Config file update
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- www/site/config/config.php
+++ www/site/config/config.php
@@ -14,7 +14,123 @@ of the system, please check out http://getkirby.com/docs/advanced/options
 
 */
 
+
+
+/*
+
+---------------------------------------
+Panel
+---------------------------------------
+
+*/
+
+
+c::set('panel.install', true);
+c::set('debug', true);
+c::set('rewrite', true);
+
+
+/*
+---------------------------------------
+Meta Tags
+---------------------------------------
+
+For use with https://github.com/pedroborges/kirby-meta-tags
+
+*/
+
+c::set('meta-tags.default', [
+    'title' => function($page) {
+        return $page->isHomePage()
+            ? site()->title()
+            : site()->title() . ' | ' . $page->title();
+    },
+    'meta' => [
+        'description' => function($page) { if($page->metadescription()){return $page->metadescription();}else{return site()->description();}; }
+    ],
+    'link' => [
+        'canonical' => function($page) { return $page->url(); }
+    ],
+    'og' => [
+        'title' => function($page) {
+            return $page->isHomePage()
+                    ? site()->title()
+                    : site()->title() . ' | ' . $page->title();
+        },
+        'description' => site()->description(),
+        'type' => 'website',
+        'site_name' => site()->title(),
+        'url' => function($page) { return $page->url(); },
+        'image' => function($page) { return $page->twittercard()->toFile() ? $page->twittercard()->toFile()->url() : page('home')->twittercard()->toFile()->url(); }
+    ],
+    'twitter' => [
+    'card' => 'summary',
+    'site' => site()->title(),
+    'title' => site()->title(),
+    'description' => site()->description(),
+    'namespace:image' => function($site) {
+        return [
+            'image' => function($page) { return $page->twittercard()->toFile() ? $page->twittercard()->toFile()->url() : page('home')->twittercard()->toFile()->url(); },
+            'alt' => function($page) {       
+                return $page->isHomePage()
+                    ? site()->title()
+                    : site()->title() . ' | ' . $page->title();
+            }
+        ];
+    }
+]
+]);
+
+
+
+
+/*
+
+---------------------------------------
+SSL
+---------------------------------------
+
+*/
+
+//c::set('ssl', true);
+
+
+/*
+
+---------------------------------------
+Cache
+---------------------------------------
+
+*/
+
+
+// c::set('cache', true);
+// c::set('cache.driver', 'file');
+//c::set('cache.ignore', array('sitemap'));
+
+
+/*
+
+---------------------------------------
+Panel CSS
+---------------------------------------
+
+*/
+
+
+// c::set('panel.stylesheet', 'assets/styles/panel.css');
+
+
+/*
+
+---------------------------------------
+Blade Directives
+---------------------------------------
+
+*/
+
 c::set('blade.directives', [
 	'customhtml' => function($value){return "<p>Some custom HTML</p>";},
 	'testdir' => function($value){ return '<p>TEST DIRECTIVE</p>';}
 ]);
+
